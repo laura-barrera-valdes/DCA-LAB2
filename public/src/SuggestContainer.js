fc554c1d2234810948642a5286b9c54bd8845aba
laura-barrera-valdes <laurabv99@gmail.com>
2023-03-11 00:25:38 -0500

    feat: nav and stories

--- public/src/SuggestContainer.js
+++ public/src/SuggestContainer.js
@@ -1,6 +1,34 @@
 import * as components from "./components/export.js";
 import data from "./components/data.js"
 
+class PostContainer extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow({mode: 'open' })
+    }
+
+    connectedCallback(){/* recibe de Post */
+        this.render();
+    }
+
+    render(){
+        const compts = data.map (({profph, nickname, post, likes, views, descr, comments, date}) =>
+        `<my-post
+        profph="${profph}"
+        nickname="${nickname}"
+        post="${post}"
+        likes="${likes}"
+        views="${views}"
+        descr="${descr}"
+        comments="${comments}"
+        date="${date}"
+        ></my-post>`)//recibe las variables que le meti en post
+        this.shadowRoot.innerHTML = compts.join("")//añade los elementos de mi data en este caso
+    }
+}
+
+customElements.define('post-container', PostContainer);
+
 class myuserContainer extends HTMLElement{
     constructor(){
         super();
