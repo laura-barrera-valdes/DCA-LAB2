5bd8bfa440c46cc29ea70f41ae395e529769ead6
laura-barrera-valdes <laurabv99@gmail.com>
2023-03-10 23:47:32 -0500

    feat: data

---
+++ public/src/components/dataPost.js
@@ -0,0 +1,36 @@
+const dataPost = [
+    {
+        "id": 1,
+        "profph": "./assets/id1/Profile-Image.png",
+        "nickname": "mofi_bizcocha",
+        "post": "./assets/id1/Post-Image.png",
+        "likes": "11.000 Likes",
+        "views": "11.000 Views",
+        "descr": "I'm pifeta",
+        "comments": "View all 10.992 comments",
+        "date": "10 seconds ago",
+    },
+    {
+        "id": 2,
+        "profph": "./assets/id2/Profile-Image.png",
+        "nickname": "roro_piroro",
+        "post": "./assets/id2/Post-Image.jpg",
+        "likes": "11.999 Likes",
+        "views": "11.999 Views",
+        "descr": "A A A A A!",
+        "comments": "View all 11.900 comments",
+        "date": "2 seconds ago",
+    },
+    {
+        "id": 3,
+        "profph": "./assets/id3/Profile-Image.png",
+        "nickname": "hanna_banana",
+        "post": "./assets/id3/Post-Image.jpg",
+        "likes": "9.000 Likes",
+        "views": "9.000 Views",
+        "descr": "WAW :p",
+        "comments": "View all 8.050 comments",
+        "date": "1 hour ago",
+    }
+]
+export default dataPost;
